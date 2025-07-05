57c8f2262d323dcea340fdce9164a5839e9a4a3e
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-06 00:04:14 +0900

    て

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,37 +1,36 @@
-// 必要なモジュールをインポート
 import ScenarioManager from '../core/ScenarioManager.js';
 import { handleCharaShow } from '../handlers/chara_show.js';
-//import { handleCharaHide } from '../handlers/chara_hide.js';
+import { handleCharaHide } from '../handlers/chara_hide.js';
 import { handlePageBreak } from '../handlers/p.js';
-// GameSceneクラスを定義し、デフォルトとしてエクスポート
-export default class GameScene extends Phaser.Scene {
-   constructor() {
-        // 親クラスのコンストラクタを呼び出す
-        super('GameScene');
 
-        // プロパティの初期化
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super('GameScene');
         this.scenarioManager = null;
         this.layer = { background: null, character: null, message: null };
+        
+        // ★★★ constructorでプロパティを初期化しておくのが安全 ★★★
+        this.charaDefs = null; 
     }
 
-    // ★★★ initメソッドを追加 ★★★
-    // 他のシーンからデータを受け取るためのメソッド
+    /**
+     * PreloadSceneからデータを受け取るためのメソッド
+     * createの前にPhaserによって自動的に呼ばれる
+     */
     init(data) {
-        // PreloadSceneから渡されたキャラクター定義を受け取る
-        this.charaDefs = data.charaDefs;
+        console.log("GameScene: initデータ受け取り", data);
+        this.charaDefs = data.charaDefs; // ここでプロパティにセット
     }
 
-    // アセットのプリロードを行うメソッド
-    // preloadメソッドは大幅にスリム化
-     // ★★★ preloadからWebフォント関連を削除 ★★★
     preload() {
         console.log("GameScene: 準備中...");
         this.load.text('scene1', 'assets/scene1.ks');
     }
 
-    // ★★★ createをstartGameに統合し、シンプルにする ★★★
-    create(data) {
-        // initメソッドはcreateの前に呼ばれるので、this.charaDefsはここで使える
+    /**
+     * シーンが生成された時に呼ばれるメソッド
+     */
+    create() {
         console.log("GameScene: ゲーム開始！");
         this.cameras.main.setBackgroundColor('#000000');
         
@@ -39,6 +38,7 @@ export default class GameScene extends Phaser.Scene {
         this.layer.character = this.add.container(0, 0);
         this.layer.message = this.add.container(0, 0);
 
+        // ★★★ initでセットされた this.charaDefs を使う ★★★
         this.scenarioManager = new ScenarioManager(this, this.layer, this.charaDefs);
         
         this.scenarioManager.registerTag('chara_show', handleCharaShow);
