27941645b964ff820a46bdf64af2e44d1545caf6
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-06 00:56:21 +0900

    ウエイト

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,7 +4,7 @@ import ScenarioManager from '../core/ScenarioManager.js';
 import { handleCharaShow } from '../handlers/chara_show.js';
 //import { handleCharaHide } from '../handlers/chara_hide.js'; // これを有効にする
 import { handlePageBreak } from '../handlers/p.js';
-//import { handleWait } from '../handlers/wait.js'; // これも有効にする
+import { handleWait } from '../handlers/wait.js'; // これも有効にする
 
 export default class GameScene extends Phaser.Scene {
     constructor() {
@@ -36,7 +36,7 @@ export default class GameScene extends Phaser.Scene {
         this.scenarioManager.registerTag('chara_show', handleCharaShow);
        // this.scenarioManager.registerTag('chara_hide', handleCharaHide); // 有効化
         this.scenarioManager.registerTag('p', handlePageBreak);
-       // this.scenarioManager.registerTag('wait', handleWait); // 有効化
+        this.scenarioManager.registerTag('wait', handleWait); // 有効化
         
         this.scenarioManager.load('scene1');
         this.input.on('pointerdown', () => { this.scenarioManager.onClick(); });
