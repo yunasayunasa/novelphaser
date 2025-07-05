ece519385d7c6b5ae22d4513b8c04e2d02242bc0
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-06 01:45:54 +0900

    て

--- src/core/ScenarioManager.js
+++ src/core/ScenarioManager.js
@@ -1,5 +1,5 @@
-export default class ScenarioManager {export default class ScenarioManager {
-    /**
+export default class ScenarioManager {
+    
      * @param {Phaser.Scene} scene - このマネージャーが属するPhaserのシーン
      * @param {Object} layers - ゲームの各レイヤー（コンテナ）
      * @param {Object} charaDefs - キャラクター定義情報
