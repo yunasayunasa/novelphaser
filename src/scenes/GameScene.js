348507262aa0cd49423cff7939888deadddd9181
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-06 01:03:47 +0900

    インボマーと

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,9 +2,9 @@
 
 import ScenarioManager from '../core/ScenarioManager.js';
 import { handleCharaShow } from '../handlers/chara_show.js';
-//import { handleCharaHide } from '../handlers/chara_hide.js'; // これを有効にする
+import { handleCharaHide } from '../handlers/chara_hide.js'; // これを有効にする
 import { handlePageBreak } from '../handlers/p.js';
-//import { handleWait } from '../handlers/wait.js'; // これも有効にする
+import { handleWait } from '../handlers/wait.js'; // これも有効にする
 
 export default class GameScene extends Phaser.Scene {
     constructor() {
@@ -34,9 +34,9 @@ export default class GameScene extends Phaser.Scene {
         this.scenarioManager = new ScenarioManager(this, this.layer, this.charaDefs);
        
         this.scenarioManager.registerTag('chara_show', handleCharaShow);
-       // this.scenarioManager.registerTag('chara_hide', handleCharaHide); // 有効化
+        this.scenarioManager.registerTag('chara_hide', handleCharaHide); // 有効化
         this.scenarioManager.registerTag('p', handlePageBreak);
-      //  this.scenarioManager.registerTag('wait', handleWait); // 有効化
+        this.scenarioManager.registerTag('wait', handleWait); // 有効化
         
         this.scenarioManager.load('scene1');
         this.input.on('pointerdown', () => { this.scenarioManager.onClick(); });
