3eaf86fc1842b580549dcd733950a41de308bf23
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-07-08 13:56:22 +0900

    Update Layout.js

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,6 +1,5 @@
-// 画面サイズを引数に取り、各位置の座標を返すオブジェクト
 export const Layout = {
-    // 縦向き (portrait) のレイアウト
+    // 縦向き (portrait)
     portrait: {
         width: 720,
         height: 1280,
@@ -9,16 +8,44 @@ export const Layout = {
             left:   { x: 180, y: 800 },
             center: { x: 360, y: 800 },
             right:  { x: 540, y: 800 }
+        },
+        ui: {
+            messageWindow: {
+                y: 1100, // ウィンドウの中心Y座標
+                padding: 35
+            },
+            menuButton:    {
+                x: 660, // 画面右端から少し内側
+                y: 50
+            },
+            choiceButton: {
+                startY: 500, // 選択肢ボタンが表示され始めるY座標
+                stepY: 100   // 各ボタンのY座標の間隔
+            }
         }
     },
-    // 横向き (landscape) のレイアウト
+    // 横向き (landscape)
     landscape: {
         width: 1280,
         height: 720,
         character: {
-            left:   { x: 280, y: 450 },
+            left:   { x: 320, y: 450 },
             center: { x: 640, y: 450 },
-            right:  { x: 1000, y: 450 }
+            right:  { x: 960, y: 450 }
+        },
+        ui: {
+            messageWindow: {
+                y: 600,
+                padding: 35
+            },
+            menuButton:    {
+                x: 1220,
+                y: 50
+            },
+            choiceButton: {
+                startY: 200,
+                stepY: 90
+            }
         }
     }
 };
